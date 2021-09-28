b7f01c9b7d3912bbfa690a5d65258a69140070be
shinaz <shinattey10@gmail.com>
2021-09-28 15:54:21 +0500

    commit 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,35 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
+
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
-
+import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
+import Button from './Button';
 // Hook
-
+import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { state, loading, error } = useHomeFetch();
 
-  return <div>Home Page</div>
+console.log(state);
+
+  return (
+    <>
+      {!searchTerm && state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
