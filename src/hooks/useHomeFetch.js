c070bfb240ff119521894a59e1e7ddfd10c08b64
shinaz <shinattey10@gmail.com>
2021-10-02 15:42:48 +0500

    6 check

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -42,11 +42,12 @@ export const useHomeFetch = () => {
       const sessionState = isPersistedState('homeState');
 
       if (sessionState) {
+        console.log('Grabbing from sessionStorage');
         setState(sessionState);
         return;
       }
     }
-   
+    console.log('Grabbing from API');
     setState(initialState);
     fetchMovies(1), searchTerm;
   }, [searchTerm]);
